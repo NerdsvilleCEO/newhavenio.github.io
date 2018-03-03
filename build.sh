a51da0ab3949c7fc3f32bd69d7ecb27428ac08e1
Josh Santos <josh@nerdsville.net>
2018-03-03 13:02:24 -0500

    Cleanup build script

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 set -e
 
-DEPLOY_REPO="https://${DEPLOY_BLOG_TOKEN}@github.com/NerdsvilleCEO/newhavenio.github.io.git"
-
 function main {
 	  clean
-	  get_current_site
 	  build_site
-    deploy
 }
 
 function clean {
@@ -16,38 +12,10 @@ function clean {
 	  if [ -d "_site" ]; then rm -Rf _site; fi
 }
 
-function get_current_site {
-	  echo "getting latest site"
-	  git clone -b gh-pages --depth 1 $DEPLOY_REPO _site || true
-}
-
 function build_site {
 	  echo "building site"
     npm run build
 	  bundle exec jekyll build
 }
-
-function deploy {
-	  echo "deploying changes"
-
-	  if [ -z "$TRAVIS_PULL_REQUEST" ]; then
-	      echo "except don't publish site for pull requests"
-	      exit 0
-	  fi
-
-	  if [ "$TRAVIS_BRANCH" != "master" ]; then
-	      echo "except we should only publish the master branch. stopping here"
-	      exit 0
-	  fi
-
-	  git config --global user.name "Travis CI"
-    git config --global user.email travis@newhaven.io
-	  cd _site
-	  git add -A
-	  git status
-	  git commit -m "Latest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
-	  git push $DEPLOY_REPO gh-pages
-}
-
 
 main
